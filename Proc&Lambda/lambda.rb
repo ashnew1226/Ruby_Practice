c4d2adcd5cd36c1d1dbb50189fd6475f7dd9fd0d
ashnew1226 <ashishdede05@gmail.com>
2023-09-15 11:14:04 +0530

    final code

--- Proc&Lambda/lambda.rb
+++ Proc&Lambda/lambda.rb
@@ -9,8 +9,30 @@
 # end
 # lambda_function(my_lambda)
 
-proc = proc {|a, b| puts "first is : #{a} and second is : #{b.class}"}
-lam = lambda {|a,b| puts "first is #{a} and second is : #{b}"}
+# proc = proc {|a, b| puts "first is : #{a} and second is : #{b.class}"}
+# lam = lambda {|a,b| puts "first is #{a} and second is : #{b}"}
 
-proc.call(1)
-lam.call(1,2)
+# proc.call(1)
+# lam.call(1,2)
+
+# a = [1,2,[3,4],[5,6,7]]
+# b = []
+# a.each do |ele|
+#   if ele.class == Array
+#     ele.each do |i|
+#       b << i
+#     end
+#   else 
+#     b << ele
+#   end
+# end
+# p b
+
+string = "aaddcbbb" # output = 2a3b1c2d
+count = 0
+a = string.split('').sort!
+hash = Hash.new(0)
+a.each do |char|
+  hash[char] += 1
+end
+print hash
